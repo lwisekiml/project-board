ec94a22cdebb1d3b29841dbeddec5aafa7184fe8
wisekim <lwisekiml@naver.com>
2024-02-13 23:44:43 +0900

    #18 - 게시글 서비스 로직의 테스트와 골격 잡기
    다음의 기능을 테스트로 표현
    
    * 게시글 검색하면 게시글 리스트 반환
    * 게시글을 ID로 조회
    * 게시글 생성
    * 게시글 수정
    * 게시글 삭제

---
+++ src/test/java/com/example/projectboard/service/ArticleServiceTest.java
@@ -0,0 +1,92 @@
+package com.example.projectboard.service;
+
+import com.example.projectboard.domain.Article;
+import com.example.projectboard.domain.type.SearchType;
+import com.example.projectboard.dto.ArticleDto;
+import com.example.projectboard.dto.ArticleUpdateDto;
+import com.example.projectboard.repository.ArticleRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.*;
+
+@DisplayName("비즈니스 로직 - 게시글")
+@ExtendWith(MockitoExtension.class)
+class ArticleServiceTest {
+
+    @InjectMocks private ArticleService sut; // System Under Test (테스트 짤 때 많이 사용하는 네이밍/이 변수가 테스트 대상이다)
+
+    @Mock private ArticleRepository articleRepository;
+
+    @DisplayName("게시글을 검색하면, 게시글 리스트를 반환한다.")
+    @Test
+    void givenSearchParameters_whenSearchingArticles_thenReturnsArticleList() {
+        // Given
+
+        // When
+        Page<ArticleDto> articles = sut.searchArticles(SearchType.TITLE, "search keyword");
+
+        // Then
+        assertThat(articles).isNotNull();
+    }
+
+    @DisplayName("게시글을 조회하면, 게시글을 반환한다.")
+    @Test
+    void givenArticleId_whenSearchingArticle_thenReturnsArticle() {
+        // Given
+
+        // When
+        ArticleDto articles = sut.searchArticle(1L);
+
+        // Then
+        assertThat(articles).isNotNull();
+    }
+
+    @DisplayName("게시글 정보를 입력하면, 게시글을 생성한다.")
+    @Test
+    void givenArticleInfo_whenSavingArticle_thenSavesArticle() {
+        // given
+        given(articleRepository.save(any(Article.class))).willReturn(null);
+
+        // when
+        sut.saveArticle(ArticleDto.of(LocalDateTime.now(), "wisekim", "title", "content", "hashtag"));
+
+        // then
+        then(articleRepository).should().save(any(Article.class)); // 구현 코드를 작성하지 않아 실패
+    }
+
+    @DisplayName("게시글의 ID와 수정 정보를 입력하면, 게시글을 수정한다")
+    @Test
+    void givenArticleIdAndModifiedInfo_whenUpdatingArticle_thenUpdatesArticle() {
+        // Given
+        given(articleRepository.save(any(Article.class))).willReturn(null);
+
+        // When
+        sut.updateArticle(1L, ArticleUpdateDto.of("title", "content", "#java"));
+
+        // Then
+        then(articleRepository).should().save(any(Article.class)); // 구현 코드를 작성하지 않아 실패
+    }
+
+    @DisplayName("게시글의 ID를 입력하면, 게시글을 삭제한다")
+    @Test
+    void givenArticleId_whenDeletingArticle_thenDeletesArticle() {
+        // Given
+        willDoNothing().given(articleRepository).delete(any(Article.class)); // 아무것도 하지 않는 코드라 나중에 발전시켜야 함
+
+        // When
+        sut.deleteArticle(1L);
+
+        // Then
+        then(articleRepository).should().delete(any(Article.class));
+    }
+}
